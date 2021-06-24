bcd3d115e1f662b35c3c5e395c6f23edff901ee1
18Blank23 <85660344+18Blank23@users.noreply.github.com>
2021-06-24 13:24:24 +0800

    Update login.php

--- login.php
+++ login.php
@@ -29,33 +29,33 @@
 <body>
 
 <div class="fixed-top">
-  <header class="topbar">
-      <div class="container">
-        <div class="row">
-          <!-- social icon-->
-          <div class="col-sm-12">
-            <ul class="social-network">
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-linkedin"></i></a></li>
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-pinterest"></i></a></li>
-              <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-google-plus"></i></a></li>
-			  
-            </ul>
-	
-          </div>
-
-        </div>
-      </div>
-  </header>
+  
   <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear" >
     <div class="container" >
-      <a class="navbar-brand" href="index.php" style="text-transform: uppercase; font-size:35px;"> CRT Online Counseling</a>
+      <a class="navbar-brand" href="index.php" style="text-transform: uppercase; font-size:29px;"> CRT Online Counseling</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto" style="float: right;">
 
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+  <a class="nav-link" href="index.php">Home
+	<span class="sr-only">(current)</span>
+  </a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;" >
+  <a class="nav-link" href="about.php">About</a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="contact.php">Vision and Mission</a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="login.php">Admin</a>
+</li>
       
       </div>
     </div>
@@ -84,7 +84,7 @@
 		<h2 class="form-title">Login</h2>
 					<div class="form-group">
 				<!--	<label for="usr"><h3>User Id:</h3></label>-->
-                            <input type="text" class="form-input" name="usr" id="usr" placeholder="User id" autocomplete="off"/>
+                            <input type="text" class="form-input" name="usr" id="usr" placeholder="Username" autocomplete="off"/>
                         </div>
 
 					<div class="form-group">
