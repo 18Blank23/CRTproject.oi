1fe24e8b1cb74ab6a892866afa750705388e29be
18Blank23 <85660344+18Blank23@users.noreply.github.com>
2021-06-27 20:27:34 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Online Counseling</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/roboto/roboto.css">
+	<link rel="stylesheet" href="assets/main.css">
+	<link type="text/css" rel="stylesheet" href="css/slideshow.css"></link>
+	<link rel="stylesheet" href="style.css">
+	<script src="js/slideshow.js"></script>
+    <script src="js/description.js"></script>
+	<meta name="theme-color" content="#42bcf4">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta charset="utf-8"> 
+	 
+
+
+	<link rel="stylesheet" href="gen.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style>
+		@import 'https://fonts.googleapis.com/css?family=Baloo+Bhai|Roboto+Condensed';
+	die();
+	</style>
+</head>
+
+<body>
+
+		
+
+	<div class="bg2">
+    <center>
+<p9><h1>Counseling</h1>
+Counseling is a collaborative process that involves the development of a confidential professional relationship that focuses on personal problems.The objective of counseling is to help you clarify issues, gain insight into your feelings and thoughts and deal effectively with problems.
+<br><br>
+<h2>CRT Now and Then</h2>
+The College for Research and Technology of Cabanatuan (CRT) started as a computer school and has been known to be the first computer school in Nueva Ecija. It pioneered in applying a different approach in education like embedding competency courses to baccalaureate course that even the more established colleges and universities followed. After the main campus was established in 1988 at Cabanatuan City, a strategic plan was laid-out by its president, Dr. Reynato C. Arimbuyutan to establish campuses in all of the four districts of Nueva Ecija. In this way, it was envisioned to contend with the growing desire of students to study in their places of abode but with the quality of education comparable to its main campus. The first Branch established after its Main Campus was the Gapan Branch which opened in 1997. Currently, it is located at Abad Santos Street, Barangay Santo Nino, Gapan City with the hope that the college-owned campus will be built on its lot along Maharlika Highway, Bayanihan, Gapan City in the near future. After three years Guimba Campus was built, located at Parairo St., Saranay Dist., Guimba, Nueva Ecija and followed by San Jose Campus located at Sanchez St., San Jose City. The Gapan, Guimba and San Jose branches are Extension Campuses of the Main Branch that offers two years Technical Courses preparatory to a degree course applying the ladderized education approach. TechVoc courses are embedded in all of its curricular courses. This system had been institutionalized by CHED and TESDA to produce more skilled students and increase the chance for employment of graduates. Presently, CRT had been given by the Department of Education permit to operate the Senior High School in all of its branches. Its expertise in TechVoc education for the past two decades had been considered. In this instance, CRT had been given permits and recognition of the trifocalized system of education in the Philippines: CHED, TESDA and DepEd. The last four years had been a tremendous moment for the college with the completion of three additional buildings in Guimba, San Jose and Cabanatuan and the Gymnasium in Gapan. Winning back to back Championship in Cheerdance Competition at the Nueva Ecija Collegiate Students League (NECSL), Championship at NECSL Women Volleyball Competition, the provincial Step-Up Dance Competition championship, and many more representing the best of a true Crtian. Also, the college witnessed the first batch of BTTE graduates who passed the Licensure Examination for Teachers (LET). All the baccalaureate degrees offered by the college: B.S. in Information Technology, B.S. in Hotel and Restaurant Management, B.S. in Tourism Management, B.S. in Accounting Technology, and Bachelor in Technical Teacher Education had been recognized by the Commission on Higher Education (CHED). Scholarships, the quality of education and the everimproving facilities provided by the college had been the main reason why CRT continuously grow and develop.
+
+ </p9>
+    </center>
+	</div>
+<div class ="bg">
+	
+	</div>
+	
+	
+	
+	<script src="assets/jquery/jquery.min.js"></script>
+	<script src="assets/popper/popper.min.js"></script>
+	<script src="assets/sweetalert/sweetalert.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/moment/moment.min.js"></script>
+	<script src="assets/moment/id.js"></script>
+	<script src="assets/chatbox.js"></script>
+	
+	<script>
+		$(document).ready(function() {
+			$('.box').chatBox({
+				api: {
+					request: 'v1/request.php',
+					send: 'v1/send.php',
+					replies: 'v1/replies.php',
+					history: 'v1/history.php',
+					destroy: 'v1/destroy.php',
+					status: 'v1/status.php',
+				},
+				sessionIndex: 'chatBoxToken',
+				status: 'online',
+				refreshInterval: 7500,
+				limit: 10,
+				updateStatusInterval: 60000,
+				thankYouDict: 'Thank you! The conversation has ended.'
+			});
+		});
+
+	</script>
+		
+		
+
+        
+	
+	
+		<div class="fixed-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear" >
+    <div class="container" >
+      <a class="navbar-brand" href="index.php" style="text-transform: uppercase; font-size:29px;"> CRT Online Counseling</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto" style="float: right;">
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+  <a class="nav-link" href="index.php">Home
+	<span class="sr-only">(current)</span>
+  </a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;" >
+  <a class="nav-link" href="about.php">About</a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="mv.php">Vision and Mission</a>
+</li>
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="login.php">Admin</a>
+</li>
+      
+      </div>
+    </div>
+  </nav>
+</div>
+
+		
+		<div class="box">
+
+</div>
+	<style>
+	
+	.bg{
+
+position: absolute;
+ height: 100%;
+ width: 100%;
+
+ background-repeat:no-repeat;
+ 		background-size:50%,50%;
+		 background-position: center,center;
+		
+		 
+ top:0;
+ position: fixed;
+}
+.bg2{
+position:absolute;
+height:120%;
+width:100%;
+background-image: linear-gradient(aqua,blue);
+top:0;
+
+}
+
+p5{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 30px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+bottom:0;
+right:110px;
+
+}
+
+p9{
+    position: absolute;
+    top:100px;
+    text-align:center;
+    left:0;
+}
+p3{
+
+
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+font-size: 70px;
+-webkit-text-stroke:2px;
+-webkit-text-fill-color:rgb(225, 225, 225);
+-webkit-animation:fill 0.5s infinite alternate;
+
+}
+p6{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 35px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+top:0;
+right:0;
+
+}
+
+@media screen and (min-width: 320px) and (max-width:480px) {
+	p3{
+
+
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+font-size: 50px;
+-webkit-text-stroke:2px;
+-webkit-text-fill-color:rgb(225, 225, 225);
+-webkit-animation:fill 0.5s infinite alternate;
+position: absolute;
+text-align:center;
+top:40px;
+left:0;
+}
+p6{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 25px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+top:0;
+right:0;
+
+}
+p5{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 20px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+bottom:0;
+right:110px;
+
+}
+
+
+}
+
+	</style>
+</body>
+</html>
+<?php
+date_default_timezone_set("Asia/Bangkok");
+?>
---
+++ mv.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Online Counseling</title>
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/roboto/roboto.css">
+	<link rel="stylesheet" href="assets/main.css">
+	<link type="text/css" rel="stylesheet" href="css/slideshow.css"></link>
+	<link rel="stylesheet" href="style.css">
+	<script src="js/slideshow.js"></script>
+    <script src="js/description.js"></script>
+	<meta name="theme-color" content="#42bcf4">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta charset="utf-8"> 
+	 
+
+
+	<link rel="stylesheet" href="gen.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style>
+		@import 'https://fonts.googleapis.com/css?family=Baloo+Bhai|Roboto+Condensed';
+	die();
+	</style>
+</head>
+
+<body>
+
+		
+
+	<div class="bg2">
+    <center>
+<p9><h1>Vision</h1>
+To be recognized and respected as one of the premier members of the academic community that provides excellence in technological education.
+
+<h1>Mission</h1>
+The College for Research & Technology of Cabanatuan is a nurturing community devoted in providing accessible and affordable technological education by being a catalyst of innovation in the promotion of holistic development to empower individuals discovers their true worth and become productive members of the society.
+    </p9>
+    </center>
+	</div>
+<div class ="bg">
+	
+	</div>
+	
+	
+	
+	<script src="assets/jquery/jquery.min.js"></script>
+	<script src="assets/popper/popper.min.js"></script>
+	<script src="assets/sweetalert/sweetalert.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/moment/moment.min.js"></script>
+	<script src="assets/moment/id.js"></script>
+	<script src="assets/chatbox.js"></script>
+	
+	<script>
+		$(document).ready(function() {
+			$('.box').chatBox({
+				api: {
+					request: 'v1/request.php',
+					send: 'v1/send.php',
+					replies: 'v1/replies.php',
+					history: 'v1/history.php',
+					destroy: 'v1/destroy.php',
+					status: 'v1/status.php',
+				},
+				sessionIndex: 'chatBoxToken',
+				status: 'online',
+				refreshInterval: 7500,
+				limit: 10,
+				updateStatusInterval: 60000,
+				thankYouDict: 'Thank you! The conversation has ended.'
+			});
+		});
+
+	</script>
+		
+		
+
+        
+	
+	
+		<div class="fixed-top">
+
+  <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear" >
+    <div class="container" >
+      <a class="navbar-brand" href="index.php" style="text-transform: uppercase; font-size:29px;"> CRT Online Counseling</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto" style="float: right;">
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+  <a class="nav-link" href="index.php">Home
+	<span class="sr-only">(current)</span>
+  </a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;" >
+  <a class="nav-link" href="about.php">About</a>
+</li>
+
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="mv.php">Vision and Mission</a>
+</li>
+<li class="nav-item active" style="margin-top:10px; margin-right:10px;">
+<a class="nav-link" href="login.php">Admin</a>
+</li>
+      
+      </div>
+    </div>
+  </nav>
+</div>
+
+		
+		<div class="box">
+
+</div>
+	<style>
+	
+	.bg{
+
+position: absolute;
+ height: 100%;
+ width: 100%;
+
+ background-repeat:no-repeat;
+ 		background-size:50%,50%;
+		 background-position: center,center;
+		
+		 
+ top:0;
+ position: fixed;
+}
+.bg2{
+position:absolute;
+height:120%;
+width:100%;
+background-image: linear-gradient(aqua,blue);
+top:0;
+
+}
+
+p5{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 30px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+bottom:0;
+right:110px;
+
+}
+
+p9{
+    position: absolute;
+    top:100px;
+    text-align:center;
+    left:0;
+}
+p3{
+
+
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+font-size: 70px;
+-webkit-text-stroke:2px;
+-webkit-text-fill-color:rgb(225, 225, 225);
+-webkit-animation:fill 0.5s infinite alternate;
+
+}
+p6{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 35px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+top:0;
+right:0;
+
+}
+
+@media screen and (min-width: 320px) and (max-width:480px) {
+	p3{
+
+
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+font-size: 50px;
+-webkit-text-stroke:2px;
+-webkit-text-fill-color:rgb(225, 225, 225);
+-webkit-animation:fill 0.5s infinite alternate;
+position: absolute;
+text-align:center;
+top:40px;
+left:0;
+}
+p6{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 25px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+top:0;
+right:0;
+
+}
+p5{
+font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+ font-size: 20px;
+ -webkit-text-stroke:1px;
+ -webkit-text-fill-color:rgb(225, 225, 225);
+ -webkit-animation:fill 0.5s infinite alternate;
+position:absolute;
+bottom:0;
+right:110px;
+
+}
+
+
+}
+
+	</style>
+</body>
+</html>
+<?php
+date_default_timezone_set("Asia/Bangkok");
+?>
